b1079df365b870c7cebedb4efc80d74e1e1da9b2
Dieste, Francisco <francisco.dieste@thomascook.es>
2019-11-11 12:37:33 +0100

    cambio en la descripcion

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hola mundo,prueba GitHub");
+            Console.WriteLine("Hola mundo,prueba GitHub modificado");
         }
     }
 }
